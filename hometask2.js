7d4c343c6cecf9e9cd162ad69f59d4049ae42f11
Max Khotsin <slimgos22@gmail.com>
2023-12-23 14:59:15 +0100

    add/ Add completed Task2

---
+++ hometask2.js
@@ -0,0 +1,91 @@
+function makeObjectDeepCopy(obj) {
+  const newObj = {};
+
+  for (let key in obj) {
+    const errorObjectType = obj[key].__proto__.constructor.name === 'Error';
+    const dateObjectType = obj[key].__proto__.constructor.name === 'Date';
+    const notPlainObject = errorObjectType || dateObjectType;
+    
+    if (typeof obj[key] === 'function') {
+      newObj[key] = obj[key];
+    } else if (Array.isArray(obj[key])) {
+      newObj[key] = [...obj[key]];
+    } else if (typeof obj[key] === 'object' && !notPlainObject) {
+      newObj[key] = makeObjectDeepCopy(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+
+  return newObj;
+}
+
+function selectFromInterval(arr, firstIntVal, secondIntVal) {
+  let resultArr = [];
+  const notValidIntVals = isNaN(firstIntVal) || isNaN(secondIntVal);
+
+  if (notValidIntVals) {
+    throw new Error('Одно из значений интервала не является валидным числом.');
+  }
+
+  const arrWithIntVals = [firstIntVal, secondIntVal];
+  const sortedIntervalValues = arrWithIntVals.sort((a, b) => a - b);
+  const minIntervalValue = sortedIntervalValues[0];
+  const maxIntervalValue = sortedIntervalValues[1];
+
+  for (let key of arr) {
+    const compareValsInArr = key >= minIntervalValue && key <= maxIntervalValue;
+
+    if (isNaN(key)) {
+      resultArr = [];
+      throw new Error('Переданое первое значение должно являться массивом с числовыми значениями.');
+    }
+
+    if (compareValsInArr) {
+      resultArr.push(key);
+    }
+  }
+
+  return resultArr;
+}
+
+const myIterable = {
+  from: 1,
+  to: 6,
+
+  [Symbol.iterator]() {
+    const checkForCurrentExist = typeof this.from === 'number' ? this.from : false;
+    const checkIfCurrentIsNotNum = !isNaN(this.from);
+    const checkForLastExist = typeof this.to === 'number' ? this.to : false;
+    const checkIfLastIsNotNum = !isNaN(this.to);
+
+    const currentExistAndNumber = checkForCurrentExist && checkIfCurrentIsNotNum;
+    const lastExistAndNumber = checkForLastExist && checkIfLastIsNotNum;
+
+    if (this.from > this.to) {
+      throw new Error('Значение to больше значения from.');
+    }
+
+    if (!currentExistAndNumber || !lastExistAndNumber) {
+      throw new Error('Значение "FROM" или "TO" отсутствует, либо не является числом');
+    }
+
+    return {
+      current: this.from,
+      last: this.to,
+
+      next() {
+        if (this.current <= this.last) {
+          return {
+            value: this.current++,
+            done: false,
+          };
+        } else {
+          return {
+            done: true,
+          };
+        }
+      }
+    }
+  }
+}
