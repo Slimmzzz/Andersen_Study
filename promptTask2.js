a854dc5d4d7c5ba67b4e662d18f81e3c887db7c3
Max Khotsin <slimgos22@gmail.com>
2023-12-25 17:13:50 +0100

    refactor/ change if else statements positions for better readability

--- promptTask2.js
+++ promptTask2.js
@@ -7,22 +7,22 @@ function checkIfNumber(value) {
 function makeCalculatedOutput() {
   const firstPrompt = prompt('Введите, пожалуйста, число.');
   
-  if (checkIfNumber(firstPrompt)) {
+  if (!checkIfNumber(firstPrompt)) {
+    console.log('Некорректный ввод!');
+  } else {
     const secondPrompt = prompt('Введите ещё одно число');
     const parsedFirstPrompt = Number(firstPrompt);
-
-    if (checkIfNumber(secondPrompt)) {
+    
+    if (!checkIfNumber(secondPrompt)) {
+      console.log('Некорректный ввод!');
+    } else {
       const secondParsedPrompt = Number(secondPrompt);
       const addition = parsedFirstPrompt + secondParsedPrompt;
       const divisor = parsedFirstPrompt / secondParsedPrompt;
       const output = `Ответ: ${addition}, ${divisor}`;
-  
+      
       console.log(output);
-    } else {
-      console.log('Некорректный ввод!');
     }
-  } else {
-    console.log('Некорректный ввод!');
   }
 }
 
