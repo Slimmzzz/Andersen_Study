c7295d1b0195ad959b41991cdc6b887ff93fe86d
Max Khotsin <slimgos22@gmail.com>
2023-12-25 17:20:17 +0100

    refactor/ delete Else blocks and change function structure

--- promptTask2.js
+++ promptTask2.js
@@ -8,22 +8,22 @@ function makeCalculatedOutput() {
   const firstPrompt = prompt('Введите, пожалуйста, число.');
   
   if (!checkIfNumber(firstPrompt)) {
-    console.log('Некорректный ввод!');
-  } else {
-    const secondPrompt = prompt('Введите ещё одно число');
-    const parsedFirstPrompt = Number(firstPrompt);
-    
-    if (!checkIfNumber(secondPrompt)) {
-      console.log('Некорректный ввод!');
-    } else {
-      const secondParsedPrompt = Number(secondPrompt);
-      const addition = parsedFirstPrompt + secondParsedPrompt;
-      const divisor = parsedFirstPrompt / secondParsedPrompt;
-      const output = `Ответ: ${addition}, ${divisor}`;
-      
-      console.log(output);
-    }
+    return console.log('Некорректный ввод!');
   }
+
+  const secondPrompt = prompt('Введите ещё одно число');
+  const parsedFirstPrompt = Number(firstPrompt);
+  
+  if (!checkIfNumber(secondPrompt)) {
+    return console.log('Некорректный ввод!');
+  }
+  
+  const secondParsedPrompt = Number(secondPrompt);
+  const addition = parsedFirstPrompt + secondParsedPrompt;
+  const divisor = parsedFirstPrompt / secondParsedPrompt;
+  const output = `Ответ: ${addition}, ${divisor}`;
+  
+  console.log(output);
 }
 
 makeCalculatedOutput();
