78cae047ee4c4088f1323710c66c2656b5fa9e3e
Max Khotsin <slimgos22@gmail.com>
2023-12-25 18:01:35 +0100

    init/ create branch for hometask4

--- hometask3_2.js
+++
@@ -1,8 +0,0 @@
-function createDebounceFunction(callback, delay) {
-  let timeoutId;
-
-  return function(...arguments) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => callback(...arguments), delay);
-  };
-}
