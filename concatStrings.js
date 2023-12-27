d1e4fe08214badf37a4118416ac365d5d15c5957
Max Khotsin <slimgos22@gmail.com>
2023-12-27 14:58:30 +0100

    add/ add files for Hometask4

---
+++ concatStrings.js
@@ -0,0 +1,20 @@
+let concatStrings = function(str, separator = '') {
+  let resultArr = [str];
+
+  return function inner(arg) {
+    const argExistAndString = arg && typeof arg === 'string';
+    if (argExistAndString || arg === '') {
+      resultArr.push(arg);
+      
+      return function(...nextFuncArg) {
+        return inner(...nextFuncArg);
+      }
+    }
+
+    if (typeof separator === 'string') {
+      return resultArr.join(separator);
+    } else {
+      return resultArr.join('');
+    }
+  }
+}
