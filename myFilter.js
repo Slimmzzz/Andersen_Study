7160f22b7aa12809f34ca58de181337a27caf041
Max Khotsin <slimgos22@gmail.com>
2024-01-01 20:46:32 +0100

    refactor/ add additional check for non-func callback

--- myFilter.js
+++ myFilter.js
@@ -2,6 +2,10 @@ Array.prototype.myFilter = function(callbackFunc, thisArg = this) {
   let resultArr = [];
 
   for (let i = 0; i < this.length; i++) {
+    if (typeof callbackFunc !== 'function') {
+      throw new Error('Первый аргумент не является функцией.');
+    }
+
     if (callbackFunc.call(thisArg, this[i], i, this)) {
       resultArr.push(this[i]);
     }
